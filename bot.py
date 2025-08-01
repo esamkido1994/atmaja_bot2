38b7dbbb37c8c07920a3fc4e97cb0724f6f0f6d7
esamkido1994 <143332759+esamkido1994@users.noreply.github.com>
2025-08-01 12:50:33 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -262,12 +262,41 @@ if (Abs == '3'):
 	print(Ba_bS+"""\n- اهلا بك عزيزي مره اخرى تم اختيار طلبم لرشق 8000 \nمتابع يرجى الانتضار الى ان يتم الوصول الى طلبك\n الطلبات الان 30 طلب 💞💞\n\n - Welcome dear, once again your request has been\nselected to throw 3000 followers Please wait\n until your request is reached Orders are now\n   50 requests 💞💞.   """)
 if (Abs == '4'):
 	print(Ba_bS+"""\n- اهلا بك عزيزي مره اخرى تم if (Abs == '4'):
-	print(Ba_bS+"""\n- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 10000 
+	if (Abs == '4'):
+    print(Ba_bS + """
+- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 10000 
 متابع يرجى الانتظار الى ان يتم الوصول الى طلبك
 الطلبات الان 40 طلب 💞💞
 
 - Welcome dear, once again your request has been
 selected to throw 10000 followers. Please wait
+until your request is reached. Orders are now
+200 requests 💞💞.
+""")
+
+if (Abs == '5'):
+    print(Ba_bS + """
+- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 15000 
+متابع يرجى الانتظار الى ان يتم الوصول الى طلبك
+الطلبات الان 50 طلب 💞💞
+
+- Welcome dear, once again your request has been
+selected to throw 15000 followers. Please wait
+until your request is reached. Orders are now
+250 requests 💞💞.
+""")
+
+if (Abs == '6'):
+    print(Ba_bS + """
+- اهلا بك عزيزي مره اخرى تم اختيار طلبك لرشق 20000 
+متابع يرجى الانتظار الى ان يتم الوصول الى طلبك
+الطلبات الان 60 طلب 💞💞
+
+- Welcome dear, once again your request has been
+selected to throw 20000 followers. Please wait
+until your request is reached. Orders are now
+2 requests 💞💞.
+""")
 until your request is reached. Orders are now
 200 requests 💞💞. """)
 
